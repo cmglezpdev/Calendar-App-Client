c275c59e3e6a4821426c26c07aa29a7912c386e8
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-08-02 02:32:22 -0400

    add calendar component

--- src/components/calendar/CalendarScreen.jsx
+++ src/components/calendar/CalendarScreen.jsx
@@ -1,9 +1,31 @@
 import { Navbar } from "../ui/Navbar"
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer = momentLocalizer(moment)
+
+const events = [
+  {
+    title: 'Boss Brithday',
+    start: moment().toDate(),
+    end: moment().add(2, 'hours').toDate(),
+    bgColor: '#fafafa',
+  }
+]
+
 
 export const CalendarScreen = () => {
   return (
-    <div>
+    <div className="flex flex-col h-screen">
       <Navbar />
+
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+      />
     </div>
   )
 }
